2b5b4fe78107ded5b855de036c380a14131250ec
Dalu Wins <60736194+dalu-wins@users.noreply.github.com>
2024-08-18 00:58:12 +0200

    Theme adjusted for splash screen

--- app/src/main/java/de/wins/plantdex/core/theme/Theme.kt
+++ app/src/main/java/de/wins/plantdex/core/theme/Theme.kt
@@ -1,6 +1,5 @@
 package de.wins.plantdex.core.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -40,7 +39,7 @@ fun PlantDexTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
