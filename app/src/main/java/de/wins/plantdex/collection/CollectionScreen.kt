53bf1d62a64dad32f9cfed665045cecaf661f393
Dalu Wins <60736194+dalu-wins@users.noreply.github.com>
2024-08-18 10:49:52 +0200

    fixed warnings

--- app/src/main/java/de/wins/plantdex/collection/CollectionScreen.kt
+++ app/src/main/java/de/wins/plantdex/collection/CollectionScreen.kt
@@ -1,4 +1,4 @@
-package de.wins.plantdex.collection;
+package de.wins.plantdex.collection
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
