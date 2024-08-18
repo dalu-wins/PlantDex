626b68a2dcf75e1e43d822bbec08a3577ace0004
Dalu Wins <60736194+dalu-wins@users.noreply.github.com>
2024-08-18 10:48:45 +0200

    navigation rail implemented

--- app/src/main/java/de/wins/plantdex/MyScreen.kt
+++ app/src/main/java/de/wins/plantdex/MyScreen.kt
@@ -1,11 +1,11 @@
 package de.wins.plantdex
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import de.wins.plantdex.core.navigation.MyNavHost
 import de.wins.plantdex.core.navigation.bar.MyNavigationBar
 import de.wins.plantdex.core.navigation.rail.MyNavigationRail
@@ -21,16 +21,16 @@ fun MyScreen(
     showNavigationBar: Boolean,
     showNavigationRail: Boolean
 ) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Scaffold(
-            bottomBar = { if (showNavigationBar) MyNavigationBar() },
-        ) { innerPaddingValues ->
-            MyNavHost(innerPaddingValues)
-        }
-        if (showNavigationRail) MyNavigationRail()
+    var modifier = Modifier.fillMaxSize()
+    if (showNavigationRail) {
+        MyNavigationRail()
+        modifier = modifier.padding(start = 80.dp)
     }
 
+    Scaffold(
+        modifier = modifier,
+        bottomBar = { if (showNavigationBar) MyNavigationBar() },
+    ) { innerPaddingValues ->
+        MyNavHost(innerPaddingValues)
+    }
 }
--- app/src/main/java/de/wins/plantdex/collection/CollectionScreen.kt
+++ app/src/main/java/de/wins/plantdex/collection/CollectionScreen.kt
@@ -1,7 +1,13 @@
 package de.wins.plantdex.collection;
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -9,4 +15,16 @@ fun CollectionScreen(
     navController: NavController,
     innerPaddingValues: PaddingValues
 ) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPaddingValues)
+    ) {
+        items(100) {
+            Text(
+                text = "Item$it",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
