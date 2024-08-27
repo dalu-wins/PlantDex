13debf0e8d169c3eca19f2ef37c6c1f4b84ff653
Dalu Wins <daniil.wins@gmail.com>
2024-08-27 19:44:02 +0200

    improved scanner ui

--- app/src/main/java/de/wins/plantdex/scanner/presentation/ScannerScreen.kt
+++ app/src/main/java/de/wins/plantdex/scanner/presentation/ScannerScreen.kt
@@ -4,14 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import de.wins.plantdex.R
 import de.wins.plantdex.core.presentation.MyTopBar
 import de.wins.plantdex.scanner.presentation.components.CameraControlRow
@@ -42,8 +39,7 @@ fun ScannerScreen(
         ) {
             CameraPreview(
                 modifier = Modifier
-                    .fillMaxSize(fraction = 0.95f)
-                    .clip(RoundedCornerShape(12.dp))
+                    .fillMaxSize()
                     .weight(0.7f)
             )
             CameraControlRow(
