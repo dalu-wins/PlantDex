b3b54ee42c861370bef940d3f95cd2550a25f9aa
Dalu Wins <daniil.wins@gmail.com>
2024-09-28 14:54:02 +0200

    removed offset with rounded corners, because it doesn't work on the bottom

--- app/src/main/java/de/wins/plantdex/datasheet/presentation/DatasheetSuccess.kt
+++ app/src/main/java/de/wins/plantdex/datasheet/presentation/DatasheetSuccess.kt
@@ -6,14 +6,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import de.wins.plantdex.core.domain.Plant
-import de.wins.plantdex.datasheet.presentation.components.InfoColumn
+import de.wins.plantdex.datasheet.presentation.components.InformationColumn
 
 @Composable
 fun DatasheetSuccess(plant: Plant, innerPaddingValues: PaddingValues){
@@ -30,12 +28,11 @@ fun DatasheetSuccess(plant: Plant, innerPaddingValues: PaddingValues){
             painter = painterResource(id = plant.imageId),
             contentDescription = "plant image"
         )
-        InfoColumn(
+        InformationColumn(
             plant = plant,
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(0.65f)
-                .offset(y = (-24).dp)
         )
     }
 }
--- app/src/main/java/de/wins/plantdex/datasheet/presentation/components/InformationColumn.kt
+++ app/src/main/java/de/wins/plantdex/datasheet/presentation/components/InformationColumn.kt
@@ -3,7 +3,6 @@ package de.wins.plantdex.datasheet.presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,15 +12,14 @@ import de.wins.plantdex.core.domain.Plant
 import de.wins.plantdex.theme.Typography
 
 @Composable
-fun InfoColumn(
+fun InformationColumn(
     plant: Plant,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier
             .background(
-                color = MaterialTheme.colorScheme.surfaceContainer,
-                shape = RoundedCornerShape(24.dp)
+                color = MaterialTheme.colorScheme.background
             )
             .padding(20.dp)
     ) {
